d3bbc11140040f7553a616309101632f72dd8762
0548643 <0548643@hr.nl>
2020-11-25 21:19:10 +0100

    Tags en categories

---
+++ api/src/main/java/com/FireSale/api/model/Tag.java
@@ -0,0 +1,19 @@
+package com.FireSale.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+public class Tag extends BaseEntity {
+    @Column(nullable = false, unique = true)
+    private String name;
+    @ManyToMany
+    private Set<Auction> auctions;
+}
