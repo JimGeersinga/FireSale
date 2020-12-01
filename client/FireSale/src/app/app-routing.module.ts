2b5171fce3f33a395c3f33e18a99aaadeaf68cf9
Mike van Leeuwen <mike@appinnovators.nl>
2020-12-01 11:16:20 +0100

    Initial service with mock data and changed file structure to be the same as users

--- client/FireSale/src/app/app-routing.module.ts
+++ client/FireSale/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './users/components/register/register.component';
-import { AuctionListComponent } from './auctions/auction-list/auction-list.component';
+import { AuctionListComponent } from './auctions/components/auction-list/auction-list.component';
 import { LoginComponent } from './users/components/login/login.component';
 import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent},
-  { path: '', component: AuctionListComponent, canActivate: [AuthGuard]}
+  { path: '', component: AuctionListComponent}
 ];
 
 @NgModule({
