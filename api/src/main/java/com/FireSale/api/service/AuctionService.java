86e746f6561c76b12be733fb405ed603a114718b
0548643 <0548643@hr.nl>
2020-12-02 21:32:18 +0100

    Updated PUT, added DELETE

--- api/src/main/java/com/FireSale/api/service/AuctionService.java
+++ api/src/main/java/com/FireSale/api/service/AuctionService.java
@@ -4,7 +4,7 @@ import com.FireSale.api.exception.ResourceNotFoundException;
 import com.FireSale.api.mapper.AuctionMapper;
 import com.FireSale.api.model.Auction;
 import com.FireSale.api.repository.AuctionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,26 +12,19 @@ import java.util.Collection;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AuctionService {
 
     private final AuctionRepository auctionRepository;
     private final AuctionMapper auctionMapper;
 
-    @Autowired
-    public AuctionService(AuctionRepository auctionRepository, AuctionMapper auctionMapper) {
-        this.auctionRepository = auctionRepository;
-        this.auctionMapper = auctionMapper;
-    }
-
-    // C - post (create)
     @Transactional(readOnly = false)
     public Auction create(Auction auction) {
         auctionRepository.save(auction);
         return auction;
     }
 
-    // R 1 - get all
     public Collection<Auction> getAuctions() {
         List<Auction> results = auctionRepository.findAll();
         if (results.isEmpty()) {
@@ -40,19 +33,29 @@ public class AuctionService {
         return results;
     }
 
-    // R 2 - get by id
     public Auction getAuctionById(final long id) {
         return auctionRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(String.format("No auction exists for id: %d", id), Auction.class));
     }
 
-    // U - put by id (update)
+    @Transactional(readOnly = false)
     public Auction updateAuction(Long id, Auction auction) {
-        auction.setId((id));
+        final Auction existing = auctionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("No auction exists for id: %d", id), Auction.class));
+        existing.setName(auction.getName());
+        existing.setDescription(auction.getDescription());
+        existing.setStartDate(auction.getStartDate());
+        existing.setEndDate(auction.getEndDate());
+        existing.setMinimalBid(auction.getMinimalBid());
+        existing.setIsFeatured(auction.getIsFeatured());
+        existing.setStatus(auction.getStatus());
+        existing.setBids(auction.getBids());
+        existing.setImages(auction.getImages());
+        existing.setCategories(auction.getCategories());
+        existing.setTags(auction.getTags());
         return auctionRepository.save(auction);
     }
 
-    // D - delete by id
     public void deleteAuction(Long id) {
         auctionRepository.deleteById(id);
     }
