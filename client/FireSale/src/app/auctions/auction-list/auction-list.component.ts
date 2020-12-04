6a555468fa41cb498ea324925f85e09e9fd5bc06
0548643 <0548643@hr.nl>
2020-12-04 22:18:59 +0100

    List mock auctions

--- client/FireSale/src/app/auctions/auction-list/auction-list.component.ts
+++ client/FireSale/src/app/auctions/auction-list/auction-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Auction } from './auction';
+import { AUCTIONS } from './mock-auctions';
 
 @Component({
   selector: 'app-auction-list',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuctionListComponent implements OnInit {
 
+  auctions = AUCTIONS;
+  selectedAuction: Auction;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(auction: Auction): void {
+    this.selectedAuction = auction;
+  }
+
 }
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { Hero } from '../hero';
+// import { HEROES } from '../mock-heroes';
+
+// @Component({
+//   selector: 'app-heroes',
+//   templateUrl: './heroes.component.html',
+//   styleUrls: ['./heroes.component.css']
+// })
+
+// export class HeroesComponent implements OnInit {
+
+//   heroes = HEROES;
+//   selectedHero: Hero;
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+//   onSelect(hero: Hero): void {
+//     this.selectedHero = hero;
+//   }
+// }
