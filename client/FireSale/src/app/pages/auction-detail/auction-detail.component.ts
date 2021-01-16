ab034861555120c67dfd70ee763f6bbac2862ef1
sherman <j.geersinga@outlook.com>
2021-01-16 18:44:52 +0100

    rewrite cancel (#146)

--- client/FireSale/src/app/pages/auction-detail/auction-detail.component.ts
+++ client/FireSale/src/app/pages/auction-detail/auction-detail.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
-import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { ApiResponse } from 'src/app/core/services/apiResponse';
 import { OkDialogComponent } from 'src/app/shared/components/ok-dialog/ok-dialog.component';
 import { AuctionDTO } from 'src/app/shared/models/auctionDto';
 import { CreateAuctionDTO } from 'src/app/shared/models/createAuctionDto';
-import { ProfileDto } from 'src/app/shared/models/profileDto';
 import { AuctionService } from 'src/app/shared/services/auction.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
@@ -17,66 +15,47 @@ import { UserService } from 'src/app/shared/services/user.service';
   styleUrls: ['./auction-detail.component.scss']
 })
 export class AuctionDetailComponent implements OnInit {
-  private auctionId: number;
-  private userId$: Observable<number>;
-  private startDt$: Observable<Date>;
-  private currentDate = new Date(Date.now());
-  private isCancelled$: Observable<boolean>;
-  public currentAuction;
   public model$: Observable<ApiResponse<AuctionDTO>>;
-
-  public user$: Observable<ApiResponse<ProfileDto>>;
   public canCancelAuction = false;
-  public canCancelAuction$: Observable<boolean>;
-  public disableCancelButton$ : Observable<boolean>;
-  public cancelButtonClicked$ = new BehaviorSubject<boolean>(false); 
 
+  private auctionId: number;
 
   constructor(
     private route: ActivatedRoute,
     private auctionService: AuctionService,
     private userService: UserService,
     private dialog: MatDialog
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.auctionId = params.id;
-      this.model$ = this.auctionService.getSingle(this.auctionId);
-      this.userId$ = this.model$.pipe(map(m => m.data.user.id));
-      this.startDt$ = this.model$.pipe(map(m => m.data.startDate));
-      this.isCancelled$ = this.model$.pipe(map(m => m.data.status === "CANCELLED"));
-      
-      this.canCancelAuction$ = combineLatest([
-        this.userId$,
-        this.userService.currentUser$,
-        this.startDt$,
-        this.isCancelled$,
-        this.cancelButtonClicked$])
-          .pipe(
-              map(([auctionUserId, user, startDt, isCancelled, cancelButtonClicked]) => { 
-              const isCurrentUser = auctionUserId === user.id;
-              const isAdmin = user.role === "ADMIN";
-                if(isCancelled || cancelButtonClicked){
-                  return false;
-                }
-                if(isAdmin){
-                  return true;
-                }
-              return (isCurrentUser && (startDt > this.currentDate));
-        }))
 
-        this.disableCancelButton$ = this.canCancelAuction$.pipe(
-          map(c => !c),
-          startWith(false)
-          );
-        });
+      this.loadAuction();
+    });
+  }
+
+  public loadAuction(): void {
+    this.model$ = this.auctionService.getSingle(this.auctionId);
+    this.model$.subscribe(response => {
+      const auction = response.data;
+
+      this.userService.currentUser$.subscribe(currentUser => {
+        if (!currentUser) {
+          this.canCancelAuction = false;
+          return;
+        }
+        const currentUserIsAdmin = currentUser.role === 'ADMIN';
+        this.canCancelAuction = (auction.status === 'READY' && ((currentUser.id === auction.user.id && (auction.startDate > new Date())) || currentUserIsAdmin));
+      });
+    });
   }
 
   cancelAuction(data: CreateAuctionDTO): void {
-    this.cancelButtonClicked$.next(true);
-    data.status = "CANCELLED";
-    this.auctionService.patch(this.auctionId, data).subscribe(x => {console.log('test')});
+    data.status = 'CANCELLED';
+    this.auctionService.patch(this.auctionId, data).subscribe(() => {
+      this.loadAuction();
+    });
     this.dialog.open(OkDialogComponent, { data: { title: 'Veiling annuleren', message: 'De veiling is geannuleerd' } });
-    }
+  }
 }
