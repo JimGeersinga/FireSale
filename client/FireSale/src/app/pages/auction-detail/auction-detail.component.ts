0b8592a550d784d6f20ec02cd8065e51e85e8dc7
Astrid-HR <38784984+Astrid-HR@users.noreply.github.com>
2021-01-17 12:22:54 +0100

    Mark auction as featured (#150)
    * Mark auction as featured
    
    * Removed currentUser$
    
    * Removed variable

--- client/FireSale/src/app/pages/auction-detail/auction-detail.component.ts
+++ client/FireSale/src/app/pages/auction-detail/auction-detail.component.ts
@@ -6,6 +6,8 @@ import { ApiResponse } from 'src/app/core/services/apiResponse';
 import { OkDialogComponent } from 'src/app/shared/components/ok-dialog/ok-dialog.component';
 import { AuctionDTO } from 'src/app/shared/models/auctionDto';
 import { CreateAuctionDTO } from 'src/app/shared/models/createAuctionDto';
+import { ProfileDto } from 'src/app/shared/models/profileDto';
+import { UserDto } from 'src/app/shared/models/userDto';
 import { AuctionService } from 'src/app/shared/services/auction.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
@@ -16,8 +18,9 @@ import { UserService } from 'src/app/shared/services/user.service';
 })
 export class AuctionDetailComponent implements OnInit {
   public model$: Observable<ApiResponse<AuctionDTO>>;
+  public isAdmin = false;
+  public user$: Observable<ApiResponse<ProfileDto>>;
   public canCancelAuction = false;
-
   private auctionId: number;
 
   constructor(
@@ -25,12 +28,15 @@ export class AuctionDetailComponent implements OnInit {
     private auctionService: AuctionService,
     private userService: UserService,
     private dialog: MatDialog
-  ) { }
+  ) {
+      this.userService.currentUser$.subscribe(item => {
+      this.isAdmin = item && item.role === 'ADMIN';
+    });
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.auctionId = params.id;
-
       this.loadAuction();
     });
   }
@@ -57,5 +63,11 @@ export class AuctionDetailComponent implements OnInit {
       this.loadAuction();
     });
     this.dialog.open(OkDialogComponent, { data: { title: 'Veiling annuleren', message: 'De veiling is geannuleerd' } });
+  }
+
+  markAsFeatured(data: CreateAuctionDTO): void {
+    data.isFeatured = true;
+    this.auctionService.patch(this.auctionId, data).subscribe(x => {console.log('test')});
+    this.dialog.open(OkDialogComponent, { data: { title: 'Feature', message: 'De veiling is aangemerkt als featured.' } });
   }
 }
