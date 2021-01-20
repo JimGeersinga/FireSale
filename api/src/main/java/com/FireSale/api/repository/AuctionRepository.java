63e92ddec9363565e565d949b2e5e0b140e322c3
Astrid <0949082@hr.nl>
2021-01-20 17:22:59 +0100

    Added missing countQueries in AuctionRepository

--- api/src/main/java/com/FireSale/api/repository/AuctionRepository.java
+++ api/src/main/java/com/FireSale/api/repository/AuctionRepository.java
@@ -14,55 +14,63 @@ import java.util.List;
 @Repository
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
 
-        @Query("SELECT a FROM Auction a LEFT JOIN FETCH a.bids b WHERE a.status = 'READY' AND a.endDate > current_timestamp AND a.user.id = :id AND a.isDeleted = false order by a.endDate asc")
+        @Query(value = "SELECT a FROM Auction a LEFT JOIN FETCH a.bids b WHERE a.status = 'READY' AND a.endDate > current_timestamp AND a.user.id = :id AND a.isDeleted = false order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.endDate > current_timestamp AND a.user.id = :id AND a.isDeleted = false")
         List<Auction> findActiveAuctionsByUserId(@Param("id") long userId);
 
         @Query(value = "SELECT a FROM Auction a LEFT JOIN FETCH a.bids b WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp  order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.startDate <= current_timestamp AND a.endDate > current_timestamp")
         Page<Auction> findActiveAuctions(Pageable pageable);
 
-        @Query("SELECT a FROM Auction a LEFT JOIN FETCH a.bids b WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND a.isFeatured = false order by a.endDate asc")
+        @Query(value = "SELECT a FROM Auction a LEFT JOIN FETCH a.bids b WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND a.isFeatured = false order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND a.isFeatured = false")
         Page<Auction> findActiveAuctionsByIsFeaturedFalse(Pageable pageable);
 
-        @Query("SELECT a FROM Auction a LEFT JOIN FETCH a.bids b WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND a.isFeatured = true  order by a.endDate asc")
+        @Query(value = "SELECT a FROM Auction a LEFT JOIN FETCH a.bids b WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND a.isFeatured = true  order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND a.isFeatured = true")
         List<Auction> findActiveAuctionsByIsFeaturedTrue();
 
-        @Query("SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.categories c WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND c.id in :categories  order by a.endDate asc")
+        @Query(value = "SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.categories c WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND c.id in :categories order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND c.id in :categories")
         List<Auction> findAuctionsByCategories(long[] categories);
 
-        @Query("SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.tags t WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND t.name in :tags  order by a.endDate asc")
+        @Query(value = "SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.tags t WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND t.name in :tags order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND t.name in :tags")
         List<Auction> findAuctionsByTags(String[] tags);
 
 
-        @Query("SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join FavouriteAuction fa on fa.auction.id = a.id WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND fa.user.id = :user  order by a.endDate asc")
+        @Query(value = "SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join FavouriteAuction fa on fa.auction.id = a.id WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND fa.user.id = :user order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND fa.user.id = :user")
         List<Auction> findAuctionsByFavourite(Long user);
 
-        @Query("SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND b.user.id = :user  order by a.endDate asc")
+        @Query(value = "SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND b.user.id = :user order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND b.user.id = :user")
         List<Auction> findActiveByUserBid(Long user);
 
-        @Query("SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND a.name LIKE CONCAT('%',:name,'%')  order by a.endDate asc")
+        @Query(value = "SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND a.name LIKE CONCAT('%',:name,'%') order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.isDeleted = false AND a.startDate <= current_timestamp AND a.endDate > current_timestamp AND a.name LIKE CONCAT('%',:name,'%')")
         List<Auction> findAuctionsByNameLike(String name);
 
-        @Query("SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.categories c WHERE a.status = 'READY' AND a.isDeleted = false AND "
+        @Query(value = "SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.categories c WHERE a.status = 'READY' AND a.isDeleted = false AND "
                         + "a.startDate <= current_timestamp AND a.endDate > current_timestamp AND "
-                        + "a.name LIKE CONCAT('%',:name,'%') AND c.id in :categories  order by a.endDate asc")
+                        + "a.name LIKE CONCAT('%',:name,'%') AND c.id in :categories order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.isDeleted = false AND "
+                        + "a.startDate <= current_timestamp AND a.endDate > current_timestamp AND "
+                        + "a.name LIKE CONCAT('%',:name,'%') AND c.id in :categories")
         List<Auction> findAuctionsByCategoriesLikeAndNameLike(long[] categories, String name);
 
-        @Query("SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.tags t WHERE a.status = 'READY' AND a.isDeleted = false AND "
+        @Query(value = "SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.tags t WHERE a.status = 'READY' AND a.isDeleted = false AND "
                         + "a.startDate <= current_timestamp AND a.endDate > current_timestamp AND "
-                        + "a.name LIKE CONCAT('%',:name,'%') AND t.name in :tags  order by a.endDate asc")
+                        + "a.name LIKE CONCAT('%',:name,'%') AND t.name in :tags order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.isDeleted = false AND "
+                        + "a.startDate <= current_timestamp AND a.endDate > current_timestamp AND "
+                        + "a.name LIKE CONCAT('%',:name,'%') AND t.name in :tags")
         List<Auction> findAuctionsByTagsLikeAndNameLike(String[] tags, String name);
 
-        @Query("SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.tags t join a.categories c WHERE a.status = 'READY' AND a.isDeleted = false AND "
+        @Query(value = "SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.tags t join a.categories c WHERE a.status = 'READY' AND a.isDeleted = false AND "
                         + "a.startDate <= current_timestamp AND a.endDate > current_timestamp AND "
-                        + "a.name LIKE CONCAT('%',:name,'%') AND t.name in :tags AND c.id in :categories  order by a.endDate asc")
+                        + "a.name LIKE CONCAT('%',:name,'%') AND t.name in :tags AND c.id in :categories order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.isDeleted = false AND "
+                        + "a.startDate <= current_timestamp AND a.endDate > current_timestamp AND "
+                        + "a.name LIKE CONCAT('%',:name,'%') AND t.name in :tags AND c.id in :categories")
         List<Auction> findAuctionsByTagsLikeAndCategoriesANDNameLike(String[] tags, long[] categories, String name);
 
-        @Query("SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.tags t join a.categories c WHERE a.status = 'READY' AND a.isDeleted = false AND "
+        @Query(value = "SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.tags t join a.categories c WHERE a.status = 'READY' AND a.isDeleted = false AND "
                         + "a.startDate <= current_timestamp AND a.endDate > current_timestamp AND "
-                        + "t.name in :tags AND c.id in :categories  order by a.endDate asc")
+                        + "t.name in :tags AND c.id in :categories order by a.endDate asc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'READY' AND a.isDeleted = false AND "
+                        + "a.startDate <= current_timestamp AND a.endDate > current_timestamp AND "
+                        + "t.name in :tags AND c.id in :categories")
         List<Auction> findAuctionsByTagsLikeAndCategoriesLike(String[] tags, long[] categories);
 
         List<Auction> findByUserIdAndIsDeletedFalseOrderByEndDateDesc(long userId);
-        @Query("SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.finalBid b WHERE a.status = 'CLOSED' AND a.isDeleted = false and b.user.id = :user order by a.endDate desc")
+        @Query(value = "SELECT distinct a FROM Auction a LEFT JOIN FETCH a.bids b join a.finalBid b WHERE a.status = 'CLOSED' AND a.isDeleted = false and b.user.id = :user order by a.endDate desc", countQuery = "SELECT COUNT(a) FROM Auction a WHERE a.status = 'CLOSED' AND a.isDeleted = false and b.user.id = :user")
         List<Auction> findWonByUserBid(Long user);
 }
