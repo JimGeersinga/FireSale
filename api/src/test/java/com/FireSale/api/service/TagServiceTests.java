d8d1fc7962d2ae3f820ef1705e0366d90e18a0c2
Astrid <0949082@hr.nl>
2021-01-23 18:13:59 +0100

    Small cleanup

--- api/src/test/java/com/FireSale/api/service/TagServiceTests.java
+++ api/src/test/java/com/FireSale/api/service/TagServiceTests.java
@@ -9,11 +9,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -26,6 +26,15 @@ public class TagServiceTests {
 
     @InjectMocks
     TagService tagService;
+
+    @Test
+    void shouldFindAllTagsSuccessfully(){
+
+        List<Tag> givenAllTags = tagService.getAllTags();
+        assertNotNull(givenAllTags);
+
+        verify(tagRepository).findAll();
+    }
 
     @Test
     void shouldFindTagByNameSuccessfully(){
